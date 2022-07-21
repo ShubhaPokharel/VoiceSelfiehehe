8d0ed1645be8db71cbb732861cf93a522340e588
ShubhaPokharel <93957744+ShubhaPokharel@users.noreply.github.com>
2022-07-21 18:57:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
+var SpeechRecognition = window.webkitSpeechRecognition; // the varible SpeechRecognition is holding window.webkitSpeechRecognition
+var recognition = new SpeechRecognition();// the varible recognition is holding new SpeechRecognition so that means SpeechRecognition will be an object
 
 function start() {
-    document.getElementById("textbox").innerHTML = "";
-    recognition.start();
+    document.getElementById("textbox").innerHTML = "";// in the function start the input will be empty
+    recognition.start();// when the function start starts the recognition starts
 }
 
-recognition.onresult = function (event) {
+recognition.onresult = function(event) {
     console.log(event);
     var Content = event.results[0][0].transcript;
     console.log(Content);
@@ -14,9 +14,38 @@ recognition.onresult = function (event) {
     speak();
 }
 function speak() {
-    var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    var synth = window.speechSynthesis; 
 
+     speak_data = "Taking your selfie in 3 seconds";
     var utterThis  = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function () {
+        take_snapshot();
+        save();
+    }, 3000);
 }
+
+Webcam.set({ // we are setting css in the js
+    width:360, // the width of the camera will be 360
+    height:250, //height of the camera will be 250
+    image_format : 'png', //type of img will be png
+    png_quality:100 //the quality of the img will be 100
+});
+
+camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+ data_uri +'">';
+    })
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
+
